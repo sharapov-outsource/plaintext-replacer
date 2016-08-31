d23eb307bef8e32f34f65189b29fdd6cb8a96c5c
Alexander <alexander@sharapov.biz>
2016-08-31 19:32:36 +0600

    changing vars

--- src/plaintext-replacer/Url.php
+++ src/plaintext-replacer/Url.php
@@ -13,23 +13,23 @@ class Url extends AbstractReplacer
 {
   private static function _makeUrlClickable($matches)
   {
-    $ret = '';
+    $string = '';
     $url = $matches[2];
 
     if (empty($url))
       return $matches[0];
     // removed trailing [.,;:] from URL
     if (in_array(substr($url, -1), ['.', ',', ';', ':']) === true) {
-      $ret = substr($url, -1);
+      $string = substr($url, -1);
       $url = substr($url, 0, strlen($url) - 1);
     }
 
-    return $matches[1] . '<a href="' . $url . '" rel="nofollow" target="_blank">' . $url . '</a>' . $ret;
+    return $matches[1] . '<a href="' . $url . '" rel="nofollow" target="_blank">' . $url . '</a>' . $string;
   }
 
   private static function _makeFtpClickable($matches)
   {
-    $ret = '';
+    $string = '';
     $dest = $matches[2];
     $dest = 'http://' . $dest;
 
@@ -37,11 +37,11 @@ class Url extends AbstractReplacer
       return $matches[0];
     // removed trailing [,;:] from URL
     if (in_array(substr($dest, -1), ['.', ',', ';', ':']) === true) {
-      $ret = substr($dest, -1);
+      $string = substr($dest, -1);
       $dest = substr($dest, 0, strlen($dest) - 1);
     }
 
-    return $matches[1] . '<a href="' . $dest . '" rel="nofollow" target="_blank">' . $dest . '</a>' . $ret;
+    return $matches[1] . '<a href="' . $dest . '" rel="nofollow" target="_blank">' . $dest . '</a>' . $string;
   }
 
   private static function _makeEmailClickable($matches)
@@ -51,19 +51,19 @@ class Url extends AbstractReplacer
     return $matches[1] . '<a href="mailto:' . $email . '">' . $email . '</a>';
   }
 
-  public static function convert($ret)
+  public static function convert($string)
   {
-    $ret = ' ' . $ret;
+    $string = ' ' . $string;
     // in testing, using arrays here was found to be faster
-    $ret = preg_replace_callback('#([\s>])([\w]+?://[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', 'self::_makeUrlClickable', $ret);
-    $ret = preg_replace_callback('#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', 'self::_makeFtpClickable', $ret);
-    $ret = preg_replace_callback('#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', 'self::_makeEmailClickable', $ret);
+    $string = preg_replace_callback('#([\s>])([\w]+?://[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', 'self::_makeUrlClickable', $string);
+    $string = preg_replace_callback('#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', 'self::_makeFtpClickable', $string);
+    $string = preg_replace_callback('#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', 'self::_makeEmailClickable', $string);
 
     // this one is not in an array because we need it to run last, for cleanup of accidental links within links
-    $ret = preg_replace("#(<a( [^>]+?>|>))<a [^>]+?>([^>]+?)</a></a>#i", "$1$3</a>", $ret);
-    $ret = trim($ret);
+    $string = preg_replace("#(<a( [^>]+?>|>))<a [^>]+?>([^>]+?)</a></a>#i", "$1$3</a>", $string);
+    $string = trim($string);
 
-    return $ret;
+    return $string;
   }
 
   private function __construct()
